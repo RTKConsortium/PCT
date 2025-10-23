6802da3dee709cbb2f3af16d939a7e273241c1b2
Axel Garcia <axel.garcia1997@gmail.com>
2025-10-23 17:19:20 +0200

    ENH: Adapt pctHoleFillingImageFilter to inherits from itkImageToImageFilter

--- include/pctHoleFillingImageFilter.hxx
+++ include/pctHoleFillingImageFilter.hxx
@@ -1,172 +1,91 @@
-#ifndef __SmallHoleFiller_hxx
-#define __SmallHoleFiller_hxx
+#include "pctHoleFillingImageFilter.h"
 
-#include "itkImageFileWriter.h" // For intermediate debugging output
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionIterator.h"
-#include "itkNeighborhoodIterator.h"
-#include "itkNumericTraits.h"
-#include "rtkMacro.h"
+#include <itkImageDuplicator.h>
+#include <itkImageRegionIterator.h>
+#include <itkImageRegionConstIterator.h>
+#include <itkNeighborhoodIterator.h>
+#include <itkNumericTraits.h>
+#include <ostream>
 
-template <typename TImage>
-SmallHoleFiller<TImage>::SmallHoleFiller()
+namespace pct
 {
-  this->HolePixel = itk::NumericTraits<typename TImage::PixelType>::Zero;
-  this->Image = NULL;
-  this->Output = NULL;
-}
 
-template <typename TImage>
+template <typename TInputImage, typename TOutputImage>
 void
-SmallHoleFiller<TImage>::SetImage(const TImage * image)
+HoleFillingImageFilter<TInputImage, TOutputImage>::GenerateData()
 {
-  this->Image = image;
-  this->Output = TImage::New();
-}
+  using InputImageType = TInputImage;
+  using OutputImageType = TOutputImage;
+  using PixelType = typename OutputImageType::PixelType;
 
-template <typename TImage>
-void
-SmallHoleFiller<TImage>::SetHolePixel(typename TImage::PixelType pixel)
-{
-  this->HolePixel = pixel;
-}
+  // Allocate output as a copy of input
+  auto initialDuplicator = itk::ImageDuplicator<OutputImageType>::New();
+  initialDuplicator->SetInputImage(this->GetInput());
+  initialDuplicator->Update();
+  this->GetOutput()->Graft(initialDuplicator->GetOutput());
 
-template <typename TImage>
-TImage *
-SmallHoleFiller<TImage>::GetOutput()
-{
-  return this->Output.GetPointer();
-}
-
-template <typename TImage>
-void
-SmallHoleFiller<TImage>::Fill()
-{
-  if (!this->Image)
-  {
-    std::cerr << "You must first call SetImage!" << std::endl;
-    return;
-  }
-
-  // Initialize by setting the output image to the input image.
-  DeepCopy<TImage>(this->Image, this->Output);
-  unsigned int numberOfIterations = 0;
-  while (HasEmptyPixels())
-  {
-    std::cout << "Iteration " << numberOfIterations << "..." << std::endl;
-    Iterate();
-    numberOfIterations++;
-
-    //    typedef  itk::ImageFileWriter< TImage  > WriterType;
-    //    typename WriterType::Pointer writer = WriterType::New();
-    //    std::stringstream ss;
-    //    ss << "/tmp/intermediate_" << numberOfIterations << ".mha";
-    //    writer->SetFileName(ss.str());
-    //    writer->SetInput(this->Output);
-    //    writer->Update();
-  }
-
-  std::cout << "Filling completed in " << numberOfIterations << " iterations." << std::endl;
-}
-
-template <typename TImage>
-void
-SmallHoleFiller<TImage>::Iterate()
-{
-  // Make a copy of the current intermediate output. We use this to determine which pixels were holes at the beginning
-  // of this iteration.
-  typename TImage::Pointer temporaryImage = TImage::New();
-  DeepCopy<TImage>(this->Output, temporaryImage);
-
-  // Traverse the image. When a pixel is encountered that is a hole, fill it with the average of its non-hole neighbors.
-  // Do not mark pixels filled on this iteration as known. This will result in a less accurate filling, as it favors the
-  // colors of pixels that occur earlier in the raster scan order.
-  typename TImage::SizeType radius;
+  unsigned int                       iteration = 0;
+  typename OutputImageType::SizeType radius;
   radius.Fill(1);
 
-  itk::NeighborhoodIterator<TImage> intermediateNeighborhoodIterator(
-    radius, temporaryImage, temporaryImage->GetLargestPossibleRegion());
-
-  // This iterator is used to set the output pixels.
-  itk::ImageRegionIterator<TImage> outputIterator(this->Output, this->Output->GetLargestPossibleRegion());
-
-  while (!intermediateNeighborhoodIterator.IsAtEnd())
+  while (iteration < this->m_MaximumNumberOfIterations)
   {
-    if (intermediateNeighborhoodIterator.GetCenterPixel() == this->HolePixel) // We want to fill this pixel
+    // Fast presence check: if there are no hole pixels left, exit early.
+    bool                                           hasHole = false;
+    itk::ImageRegionConstIterator<OutputImageType> checkIt(this->GetOutput(),
+                                                           this->GetOutput()->GetLargestPossibleRegion());
+    for (checkIt.GoToBegin(); !checkIt.IsAtEnd(); ++checkIt)
     {
-      typename TImage::PixelType pixelSum = itk::NumericTraits<typename TImage::PixelType>::Zero;
-      // Loop over the 8-neighorhood
-      unsigned int validPixels = 0;
-      for (unsigned int i = 0; i < 27; i++)
+      if (checkIt.Get() == this->m_HolePixelValue)
       {
-        if (i == 13) // this is the center (current) pixel, so skip it
+        hasHole = true;
+        break;
+      }
+    }
+    if (!hasHole)
+      break;
+
+    auto duplicator = itk::ImageDuplicator<OutputImageType>::New();
+    duplicator->SetInputImage(this->GetOutput());
+    duplicator->Update();
+    typename OutputImageType::Pointer snapshot = duplicator->GetOutput();
+
+    itk::NeighborhoodIterator<OutputImageType> neighbor(radius, snapshot, snapshot->GetLargestPossibleRegion());
+    itk::ImageRegionIterator<OutputImageType>  writer(this->GetOutput(), this->GetOutput()->GetLargestPossibleRegion());
+
+    const unsigned int neighborSize = neighbor.Size();
+    const unsigned int centerIndex = neighborSize / 2;
+
+    for (neighbor.GoToBegin(), writer.GoToBegin(); !neighbor.IsAtEnd(); ++neighbor, ++writer)
+    {
+      PixelType centerPixel = neighbor.GetCenterPixel();
+      if (centerPixel == this->m_HolePixelValue)
+      {
+        PixelType    sum = itk::NumericTraits<PixelType>::Zero;
+        unsigned int valid = 0;
+
+        for (unsigned int i = 0; i < neighborSize; ++i)
         {
-          continue;
-        }
-        bool                       isInBounds = false;
-        typename TImage::PixelType currentPixel = intermediateNeighborhoodIterator.GetPixel(i, isInBounds);
-        if (isInBounds)
-        {
-          if (currentPixel != this->HolePixel)
+          if (i == centerIndex)
+            continue;
+          bool      inBounds = false;
+          PixelType p = neighbor.GetPixel(i, inBounds);
+          if (inBounds && p != this->m_HolePixelValue)
           {
-            validPixels++;
-            pixelSum += currentPixel;
+            sum += p;
+            ++valid;
           }
         }
-      } // end 8-connected neighbor for
 
-      if (validPixels > 0)
-      {
-        // typename TImage::PixelType pixelAverage = static_cast<typename TImage::PixelType>(pixelSum / validPixels);
-        typename TImage::PixelType pixelAverage = static_cast<typename TImage::PixelType>(
-          pixelSum *
-          (1.0 / validPixels)); // We multiply by the reciprocal because operator/ is not defined for all types.
-        outputIterator.Set(pixelAverage);
-        // std::cout << "Set " << outputIterator.GetIndex() << " to " << pixelAverage << std::endl;
+        if (valid > 0)
+        {
+          PixelType outValue = sum / valid;
+          writer.Set(outValue);
+        }
       }
-    } // end "fill this pixel?" conditional
-
-    ++intermediateNeighborhoodIterator;
-    ++outputIterator;
-  } // end main traversal loop
-}
-
-template <typename TImage>
-bool
-SmallHoleFiller<TImage>::HasEmptyPixels()
-{
-  itk::ImageRegionConstIterator<TImage> imageIterator(this->Output, this->Output->GetLargestPossibleRegion());
-
-  while (!imageIterator.IsAtEnd())
-  {
-    if (imageIterator.Get() == this->HolePixel)
-    {
-      // std::cout << "Pixel " << imageIterator.GetIndex() << " is empty." << std::endl;
-      return true;
     }
-    ++imageIterator;
-  }
-  return false;
-}
-
-///////// This is not a member function /////////////
-/** Copy the input to the output*/
-template <typename TImage>
-void
-DeepCopy(const TImage * input, TImage * output)
-{
-  output->SetRegions(input->GetLargestPossibleRegion());
-  output->Allocate();
-
-  itk::ImageRegionConstIterator<TImage> inputIterator(input, input->GetLargestPossibleRegion());
-  itk::ImageRegionIterator<TImage>      outputIterator(output, output->GetLargestPossibleRegion());
-
-  while (!inputIterator.IsAtEnd())
-  {
-    outputIterator.Set(inputIterator.Get());
-    ++inputIterator;
-    ++outputIterator;
+    ++iteration;
   }
 }
 
-#endif
+} // namespace pct
