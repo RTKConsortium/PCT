57f3fc67dcdd0b660c44d26cf41943d08553d016
Axel Garcia <axel.garcia1997@gmail.com>
2026-01-29 17:01:18 +0100

    ENH: Run Python tests by the CI

--- test/pctOutputArgumentWrapping.py
+++ test/pctOutputArgumentWrapping.py
@@ -2,13 +2,16 @@
 
 from itk import PCT as pct
 
-pos_in = [25.0, -25.0, -110.0]
-pos_out = [-25.0, 25.0, 110.0]
-dir_in = [0.0, 0.0, 1.0]
-dir_out = [0.0, 0.0, 1.0]
 
-mlp = pct.PolynomialMLPFunction.New()
-mlp.SetPolynomialDegree(2)
-mlp.Init(pos_in, pos_out, dir_in, dir_out)
+def test_output_argument_wrapping():
+    pos_in = [25.0, -25.0, -110.0]
+    pos_out = [-25.0, 25.0, 110.0]
+    dir_in = [0.0, 0.0, 1.0]
+    dir_out = [0.0, 0.0, 1.0]
 
-print(mlp.Evaluate([-50.0, 0.0, 50.0]))
+    mlp = pct.PolynomialMLPFunction.New()
+    mlp.SetPolynomialDegree(2)
+    mlp.Init(pos_in, pos_out, dir_in, dir_out)
+
+    result = mlp.Evaluate([-50.0, 0.0, 50.0])
+    print(result)
