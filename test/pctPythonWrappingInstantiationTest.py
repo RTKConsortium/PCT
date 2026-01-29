a76d53d0e4fa55a6d89b7ec4f0250e2611d65ce3
Axel Garcia <axel.garcia1997@gmail.com>
2026-01-29 17:01:18 +0100

    ENH: Run Python tests by the CI

--- test/pctPythonWrappingInstantiationTest.py
+++ test/pctPythonWrappingInstantiationTest.py
@@ -1,27 +1,29 @@
 import itk
 from itk import PCT as pct
 
-imageType = itk.Image[itk.F, 3]
-pct.FDKDDWeightProjectionFilter[imageType].New()
-pct.FDKDDConeBeamReconstructionFilter[imageType].New()
-pct.FDKDDBackProjectionImageFilter[imageType, imageType].New()
-pct.ProtonPairsToDistanceDrivenProjection[imageType, imageType].New()
-pct.SmallHoleFiller[imageType]()
 
-imageType = itk.Image[itk.F, 4]
-pct.DDParkerShortScanImageFilter[imageType, imageType].New()
-pct.ProtonPairsToBackProjection[imageType, imageType].New()
-pct.ZengWeightedBackProjectionImageFilter[imageType].New()
-pct.SmallHoleFiller[imageType]()
+def test_python_wrapping_instantiation():
+    imageType = itk.Image[itk.F, 3]
+    pct.FDKDDWeightProjectionFilter[imageType].New()
+    pct.FDKDDConeBeamReconstructionFilter[imageType].New()
+    pct.FDKDDBackProjectionImageFilter[imageType, imageType].New()
+    pct.ProtonPairsToDistanceDrivenProjection[imageType, imageType].New()
+    pct.SmallHoleFiller[imageType]()
 
-pct.ThirdOrderPolynomialMLPFunction[itk.D].New()
-pct.ThirdOrderPolynomialMLPFunction[itk.F].New()
+    imageType = itk.Image[itk.F, 4]
+    pct.DDParkerShortScanImageFilter[imageType, imageType].New()
+    pct.ProtonPairsToBackProjection[imageType, imageType].New()
+    pct.ZengWeightedBackProjectionImageFilter[imageType].New()
+    pct.SmallHoleFiller[imageType]()
 
-pct.SchulteMLPFunction.New()
-pct.PolynomialMLPFunction.New()
+    pct.ThirdOrderPolynomialMLPFunction[itk.D].New()
+    pct.ThirdOrderPolynomialMLPFunction[itk.F].New()
 
-for t1 in [itk.F, itk.D]:
-    for t2 in [itk.F, itk.D]:
-        pct.EnergyStragglingFunctor[t1, t2]()
-        pct.BetheBlochProtonStoppingPower[t1, t2]()
-        pct.IntegratedBetheBlochProtonStoppingPowerInverse[t1, t2](1, 1)
+    pct.SchulteMLPFunction.New()
+    pct.PolynomialMLPFunction.New()
+
+    for t1 in [itk.F, itk.D]:
+        for t2 in [itk.F, itk.D]:
+            pct.EnergyStragglingFunctor[t1, t2]()
+            pct.BetheBlochProtonStoppingPower[t1, t2]()
+            pct.IntegratedBetheBlochProtonStoppingPowerInverse[t1, t2](1, 1)
